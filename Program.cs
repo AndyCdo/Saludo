617357821e7be6e15a9d19d2aef4f82edcbb1dbd
Andrea <chela355@hotmail.com>
2021-02-09 16:56:19 -0500

    se hizo la tarea

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace saludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreUsuario= "Perico";
+
+            Console.WriteLine("Hola, "+nombreUsuario);
+        }
+    }
+}
